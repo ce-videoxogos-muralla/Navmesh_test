765438fbc6835858a3afa4bdf167660de493a17a
Isaac González <46535453+isaacjgonzalez@users.noreply.github.com>
2022-05-12 11:36:32 +0200

    Refactor with states

--- Assets/Scripts/AgentEnemy.cs
+++ Assets/Scripts/AgentEnemy.cs
@@ -20,29 +20,62 @@ public class AgentEnemy : MonoBehaviour {
     }
 
     void Update() {
+        bool inRangeAndVisible = IsInRangeAndVisible();
+
+  
+        switch (state) {
+            case AgentState.Idle:
+                if (inRangeAndVisible)
+                    SetState(AgentState.Chasing);
+                break;
+            case AgentState.Chasing:
+                if (inRangeAndVisible)
+                    targetPosition = player.position;
+                else
+                    SetState(AgentState.Returning);
+                break;
+            case AgentState.Returning:
+                if (inRangeAndVisible)
+                    SetState(AgentState.Chasing);
+                else if (agent.isStopped)
+                    SetState(AgentState.Idle);
+                break;
+        }
+
+        agent.destination = targetPosition;
+    }
+
+    bool IsInRangeAndVisible() {
         float distanceToPlayer = Vector3.Distance(player.position, transform.position);
         if (distanceToPlayer < playerDetectionDistance) {
             RaycastHit hitInfo;
             Vector3 directionToTarget = (player.position - transform.position).normalized;
             
             Debug.DrawRay(transform.position + Vector3.up, directionToTarget, Color.green);
-            if (Physics.Raycast(transform.position + Vector3.up, directionToTarget, out hitInfo, distanceToPlayer, -1, QueryTriggerInteraction.Ignore)) {
-                targetPosition = initPosition;
-                
-            } else {
-                targetPosition = player.position;
-                Debug.Log("Perseguindo");
+            return Physics.Raycast(transform.position + Vector3.up, directionToTarget, out hitInfo, distanceToPlayer, -1, QueryTriggerInteraction.Ignore);
+        } 
+        return false;
+    }
+
+    void SetState(AgentState newState) {
+        if (newState != state) {
+            state = newState;
+            switch (newState) {
+                case AgentState.Idle:
+                    break;
+                case AgentState.Chasing:
+                    break;
+                case AgentState.Returning:
+                    targetPosition = initPosition;
+                    break;
             }
-        } else {
-            targetPosition = initPosition;
         }
-  
-        agent.destination = targetPosition;
     }
 
 }
 
 public enum AgentState {
-    Idle, // Esperando
-    Chasing // Persiguiendo
+    Idle,       // Esperando
+    Chasing,    // Persiguiendo
+    Returning   // Volta ao punto inicial
 }
