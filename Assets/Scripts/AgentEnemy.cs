6a42627f92ce0f95557596ebce69fd8e540c9519
Isaac González <46535453+isaacjgonzalez@users.noreply.github.com>
2022-05-12 11:17:26 +0200

    Funciona. Agent persegue por visión

--- Assets/Scripts/AgentEnemy.cs
+++ Assets/Scripts/AgentEnemy.cs
@@ -3,44 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AgentEnemy : MonoBehaviour
-{
-    public float playerDetectionDistance = 30;
+public class AgentEnemy : MonoBehaviour {
+    public float playerDetectionDistance = 50;
     NavMeshAgent agent;
     Vector3 targetPosition;
     AgentState state;
     Transform player;
     Vector3 initPosition;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    void Start() {
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
         state = AgentState.Idle;
         initPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Vector3.Distance(player.position,transform.position) < playerDetectionDistance) {
+    void Update() {
+        float distanceToPlayer = Vector3.Distance(player.position, transform.position);
+        if (distanceToPlayer < playerDetectionDistance) {
             RaycastHit hitInfo;
             Vector3 directionToTarget = (player.position - transform.position).normalized;
-            if (Physics.Raycast(transform.position + Vector3.up, directionToTarget, playerDetectionDistance, -1, QueryTriggerInteraction.Ignore)) {
+            
+            Debug.DrawRay(transform.position + Vector3.up, directionToTarget, Color.green);
+            if (Physics.Raycast(transform.position + Vector3.up, directionToTarget, out hitInfo, distanceToPlayer, -1, QueryTriggerInteraction.Ignore)) {
                 targetPosition = initPosition;
+                
+            } else {
+                targetPosition = player.position;
+                Debug.Log("Perseguindo");
             }
-            targetPosition = player.position;
         } else {
             targetPosition = initPosition;
         }
-         
+  
         agent.destination = targetPosition;
     }
+
 }
 
 public enum AgentState {
     Idle, // Esperando
-    Chasing, // Perseguindo
-
+    Chasing // Persiguiendo
 }
